36a30720cc0f5b1c966ad2293384e9b37b885d75
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-01 15:42:59 +0300

    Initial Commit

---
+++ UserDefaultWithCodable/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  UserDefaultWithCodable
+//
+//  Created by Ramill Ibragimov on 01.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserDefaultWithCodable/UserDefaultWithCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultWithCodableApp.swift
+//  UserDefaultWithCodable
+//
+//  Created by Ramill Ibragimov on 01.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultWithCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
