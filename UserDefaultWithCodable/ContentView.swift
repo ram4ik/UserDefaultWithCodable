353ae1f5264cb44f2a6bf4e4e27305e9c3031002
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-01 16:26:01 +0300

    init

--- UserDefaultWithCodable/ContentView.swift
+++ UserDefaultWithCodable/ContentView.swift
@@ -7,9 +7,52 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let secondName: String
+}
+
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var secondName = ""
+    
+    @State private var defaultUser = User(firstName: "John", secondName: "Doe")
+    
+    @State private var userDefaultsFirstName = ""
+    @State private var userDefaultsSecondName = ""
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            Group {
+                if !userDefaultsFirstName.isEmpty && !userDefaultsSecondName.isEmpty {
+                    Text("Saved user: \(userDefaultsFirstName) \(userDefaultsSecondName)")
+                        .padding()
+                }
+            }
+            Group {
+                TextField("First name", text: $firstName)
+                TextField("Second name", text: $secondName)
+            }.textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Save user data") {
+                if !firstName.isEmpty && !secondName.isEmpty {
+                    defaultUser = User(firstName: firstName, secondName: secondName)
+                    if let data = try? JSONEncoder().encode(self.defaultUser) {
+                        UserDefaults.standard.set(data, forKey: "userData")
+                    }
+                } else {
+                    if let data = try? JSONEncoder().encode(self.defaultUser) {
+                        UserDefaults.standard.set(data, forKey: "userData")
+                    }
+                }
+                if let dataFromUserDefailts = UserDefaults.standard.data(forKey: "userData") {
+                    if let decodedData = try? JSONDecoder().decode(User.self, from: dataFromUserDefailts) {
+                        userDefaultsFirstName = decodedData.firstName
+                        userDefaultsSecondName = decodedData.secondName
+                    }
+                }
+            }
+        }.padding()
     }
 }
 
